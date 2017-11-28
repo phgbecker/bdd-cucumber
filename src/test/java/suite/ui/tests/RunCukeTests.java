a6486957b1a28f54083e5cfda4cd1b76a39e95f8
Victor Hugo dos Santos <vhsantos26@gmail.com>
2017-11-28 17:43:09 -0200

    teste de framework para rodar testes em paralelo

--- src/test/java/suite/ui/tests/RunCukeTests.java
+++ src/test/java/suite/ui/tests/RunCukeTests.java
@@ -2,17 +2,25 @@ package suite.ui.tests;
 
 import org.junit.runner.RunWith;
 
+import courgette.api.CourgetteOptions;
+import courgette.api.CourgetteRunLevel;
+import courgette.api.junit.Courgette;
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
 
-@RunWith(Cucumber.class)
-@CucumberOptions( 
-		features = "classpath:features",
-		glue = "suite/ui/features", 
-		plugin = { 
-				"pretty", 
-				"json:target/cucumber-report/cucumber.json"}, 
-		tags = "~@wip"
+@RunWith(Courgette.class)
+@CourgetteOptions(
+		threads = 3, 
+		runLevel = CourgetteRunLevel.SCENARIO, 
+		rerunFailedScenarios = true, 
+		showTestOutput = true,
+		cucumberOptions = @CucumberOptions( 
+				features = "src/test/resources/features",
+				glue = "suite/ui/features", 
+				plugin = { 
+						"pretty", 
+						"json:target/cucumber-report/cucumber.json"}, 
+				tags = "~@wip"
+				) 
 		)
 public class RunCukeTests {
 
