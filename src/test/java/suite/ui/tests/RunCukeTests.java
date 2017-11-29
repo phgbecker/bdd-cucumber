6f85e2e25cd9e1d092db1c800bf7a1634f03168d
Victor Hugo dos Santos <vhsantos26@gmail.com>
2017-11-29 11:35:29 -0200

    removido configuração de execução paralela

--- src/test/java/suite/ui/tests/RunCukeTests.java
+++ src/test/java/suite/ui/tests/RunCukeTests.java
@@ -2,26 +2,12 @@ package suite.ui.tests;
 
 import org.junit.runner.RunWith;
 
-import courgette.api.CourgetteOptions;
-import courgette.api.CourgetteRunLevel;
-import courgette.api.junit.Courgette;
 import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
 
-@RunWith(Courgette.class)
-@CourgetteOptions(
-		threads = 3, 
-		runLevel = CourgetteRunLevel.SCENARIO, 
-		rerunFailedScenarios = true, 
-		showTestOutput = true,
-		cucumberOptions = @CucumberOptions( 
-				features = "src/test/resources/features",
-				glue = "suite/ui/features", 
-				plugin = { 
-						"pretty", 
-						"json:target/cucumber-report/cucumber.json"}, 
-				tags = "~@wip"
-				) 
-		)
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/features", glue = "suite/ui/features", plugin = { "pretty",
+		"json:target/cucumber-report/cucumber.json" }, tags = "~@wip")
 public class RunCukeTests {
 
 }
